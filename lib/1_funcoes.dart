bc9bce1a23a048e75027fea15452a305a4f8709d
souza99 <jpfsouza99@gmail.com>
2022-04-12 22:32:33 -0300

    Criando interface

--- lib/1_funcoes.dart
+++ lib/1_funcoes.dart
@@ -1,4 +1,14 @@
-void show(){
+import 'dart:html';
+import 'dart:io';
 
+void show() {}
+
+void operacoesDasProvas() {
+  int? inicio = 0;
+
+  while (inicio == 0) {
+    print(
+        'Escolhar uma ação: \n1-verificar aprovação \n2-verificar maior nota \n3-calcular a média');
+    var escolha = stdin.readLineSync();
+  }
 }
-

7950b99c34db6a3b50435d49cc0d19709147796f
souza99 <jpfsouza99@gmail.com>
2022-04-12 22:24:48 -0300

    Criando projeto variais anonimas

---
+++ bin/funcoesanonimas.dart
@@ -0,0 +1,5 @@
+import 'package:funcoesanonimas/funcoesanonimas.dart' as funcoesanonimas;
+
+void main(List<String> arguments) {
+  print('Hello world: ${funcoesanonimas.calculate()}!');
+}
---
+++ test/funcoesanonimas_test.dart
@@ -0,0 +1,8 @@
+import 'package:funcoesanonimas/funcoesanonimas.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
